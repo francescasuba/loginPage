fbbfc2b2d20f3606877dd20a3a92878204030af0
subaap <subaap@VDI-CECH-IC-11>
2022-10-26 16:51:11 -0400

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿Create database TestUsers;
+Create table Registration(id INT IDENTITY(1,1) PRIMARY KEY,
+username Varchar(100), password Varchar(100),
+email Varchar(100), IsActive INT);
+
+SELECT * FROM Registration;

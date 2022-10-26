1b9df222dd6ab2e20c2daeaa36581a63a50772d0
Francesca Suba <72313800+francescasuba@users.noreply.github.com>
2022-10-26 18:08:22 -0400

    made controller

---
+++ loginPage/Controllers/RegistrationController.cs
@@ -0,0 +1,39 @@
+﻿using loginPage.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+
+namespace loginPage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public RegistrationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("registration")]
+        public string registration(Registration registration)
+        {
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("UsersDB").ToString());
+            SqlCommand cmd = new SqlCommand(
+                "INSERT INTO Registration(username, password, email, IsActive) VALUES('" + registration.username + "','" + registration.password + "','" + registration.email + "','" + registration.IsActive + "')", con);
+            int i = cmd.ExecuteNonQuery();
+            if (i > 0)
+            {
+                return "Data inserted.";
+            }
+            else
+            {
+                return "Error.";
+            }
+            return "";
+        }
+    }
+}

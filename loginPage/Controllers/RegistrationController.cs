3d48d0d8c584f848bc8431be130f46d37da02782
subaap <subaap@mail.uc.edu>
2022-10-27 10:46:28 -0400

    set up html

--- loginPage/Controllers/RegistrationController.cs
+++ loginPage/Controllers/RegistrationController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlTypes;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace loginPage.Controllers
 {
@@ -24,7 +25,9 @@ namespace loginPage.Controllers
             SqlConnection con = new SqlConnection(_configuration.GetConnectionString("UsersDB").ToString());
             SqlCommand cmd = new SqlCommand(
                 "INSERT INTO Registration(username, password, email, IsActive) VALUES('" + registration.username + "','" + registration.password + "','" + registration.email + "','" + registration.IsActive + "')", con);
+            con.Open();
             int i = cmd.ExecuteNonQuery();
+            con.Close();
             if (i > 0)
             {
                 return "Data inserted.";
@@ -34,6 +37,24 @@ namespace loginPage.Controllers
                 return "Error.";
             }
             return "";
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public string login(Registration registration)
+        {
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("UsersDB").ToString());
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Registration WHERE email = '" + registration.email +"' AND password = '" + registration.password+"' AND IsActive = 1", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if(dt.Rows.Count > 0)
+            {
+                return "Valid User";
+            }
+            else
+            {
+                return "Invalid User";
+            }
         }
     }
 }
